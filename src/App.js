7399e7612a71007221448545e1f1eeea5b5784d1
jeronim-j <jeronim.j@o2.pl>
2020-08-31 10:19:17 +0200

    Started structure in React Router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import {Link, Route, Switch} from 'react-router-dom'
 
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Sign from "./components/Sign";
+import Logout from "./components/Logout";
 
 const App = () => {
 
-    return null;
-
+    return (
+        <>
+            <div>
+                <Link to="/">Home</Link>
+                <Link to="/logowanie">Login</Link>
+                <Link to="/rejestracja">Sign Up</Link>
+            </div>
+            <Switch>
+                <Route component={Home} path="/"/>
+                <Route component={Login} path="logowanie"/>
+                <Route component={Sign} exact="/rejestracja"/>
+                <Route component={Logout} path="/wylogowano"/>
+            </Switch>
+        </>
+    )
 }
 
 export default App;
---
+++ src/components/Sign.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Sign = () => {
+    return <h1>Sign</h1>
+}
+
+export default Sign;

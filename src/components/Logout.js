23c34bfa9982261e11e92d80c6a3dd735fa849dd
jeronim-j <jeronim.j@o2.pl>
2020-09-03 20:26:11 +0200

    Logout page created

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,7 +1,29 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 const Logout = () => {
-    return <h1>Logout</h1>
+    return (
+        <>
+            <header className="header-main" id="header">
+                <section className="navbar-upper">
+                    <Link to="/logowanie">Zaloguj</Link>
+                    <Link to="/rejestracja">Załóż Konto</Link>
+                </section>
+                <section className="navbar-lower">
+                    <Link to="/">Start</Link>
+                    <Link to="/">O co chodzi?</Link>
+                    <Link to="/">O nas</Link>
+                    <Link to="/">Fundacja i oragnizacje</Link>
+                    <Link to="/">Kontakt</Link>
+                </section>
+            </header>
+            <section className="logout">
+                <h2 className="logout-header">Wylogowanie nastąpiło pomyślnie!</h2>
+                <div className="logout-decoration" style={{backgroundImage: `url(${process.env.PUBLIC_URL}/assets/Decoration.svg`}}/>
+                <Link className="logout-link" to="/">Strona główna</Link>
+            </section>
+        </>
+    )
 }
 
 export default Logout;

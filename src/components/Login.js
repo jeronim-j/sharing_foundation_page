863f25d181e721d47a5f1b8ae75e0a44ae67c100
jeronim-j <jeronim.j@o2.pl>
2020-09-05 17:30:03 +0200

    Fixed issue with empty forms

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,8 +2,6 @@ import React, {useState} from "react";
 import {Link} from 'react-router-dom';
 
 const Login = () => {
-    const emailInput = document.querySelector("#email");
-    const passwordInput = document.querySelector("#password");
     const [email, setEmail] = useState("");
     const [emailErr, setEmailErr] = useState("");
     const [password, setPassword] = useState("");
@@ -13,15 +11,11 @@ const Login = () => {
     const validate = () => {
         if (!email.includes('@') || !email.includes('.')) {
             setEmailErr(prevState => "Podany email jest nieprawidłowy");
-            emailInput.style.borderBottomColor = "red";
             return false;
-        } else if (password.length < 6){
+        } else if (password.length < 6) {
             setPasswordErr(prevState => "Podane hasło jest nieprawidłowe");
-            passwordInput.style.borderBottomColor = "red";
             return false;
         } else {
-            emailInput.style.borderBottomColor = "#3C3C3C";
-            passwordInput.style.borderBottomColor = "#3C3C3C";
             return true;
         }
     };
@@ -56,13 +50,28 @@ const Login = () => {
             </header>
             <section className="login">
                 <h2 className="login-header">Zaloguj się</h2>
-                <div className="login-decoration" style={{backgroundImage: `url(${process.env.PUBLIC_URL}/assets/Decoration.svg`}}/>
+                <div className="login-decoration"
+                     style={{backgroundImage: `url(${process.env.PUBLIC_URL}/assets/Decoration.svg`}}/>
                 <form className="login-form" onSubmit={handleSubmit} noValidate>
                     <label htmlFor="email">Email</label>
-                    <input id="email" type="email" name="email" value={email} onChange={e=> setEmail(e.target.value)}/>
+                    <input
+                        className={emailErr !== "" ? "error-input" : ""}
+                        id="email"
+                        type="email"
+                        name="email"
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                    />
                     <div className="error-msg">{emailErr}</div>
                     <label htmlFor="password">Hasło</label>
-                    <input id="password" type="password" name="password" value={password} onChange={e=> setPassword(e.target.value)}/>
+                    <input
+                        className={passwordErr !== "" ? "error-input" : ""}
+                        id="password"
+                        type="password"
+                        name="password"
+                        value={password}
+                        onChange={e => setPassword(e.target.value)}
+                    />
                     <div className="error-msg">{passwordErr}</div>
                     <div className="login-buttons">
                         <Link to="/rejestracja">Załóż Konto</Link>
--- src/components/Sign.js
+++ src/components/Sign.js
@@ -2,11 +2,6 @@ import React, {useState} from "react";
 import {Link} from 'react-router-dom';
 
 const Sign = () => {
-
-    const emailInput = document.querySelector("#email");
-    const passwordInput = document.querySelector("#password");
-    const passwordRepeatInput = document.querySelector("#passwordRepeat");
-    
     const [email, setEmail] = useState("");
     const [emailErr, setEmailErr] = useState("");
     const [password, setPassword] = useState("");
@@ -17,19 +12,13 @@ const Sign = () => {
     const validate = () => {
         if (!email.includes('@') || !email.includes('.')) {
             setEmailErr(prevState => "Podany email jest nieprawidłowy");
-            emailInput.style.borderBottomColor = "red";
             return false;
-        } else if (password.length < 6){
+        } else if (password.length < 6) {
             setPasswordErr(prevState => "Podane hasło jest nieprawidłowe");
-            passwordInput.style.borderBottomColor = "red";
             return false;
         } else if (password !== passwordRepeat) {
             setPasswordRepeatErr(prevState => "Hasła muszą być takie same");
-            passwordRepeatInput.style.borderBottomColor = "red";
         } else {
-            emailInput.style.borderBottomColor = "#3C3C3C";
-            passwordInput.style.borderBottomColor = "#3C3C3C";
-            passwordRepeatInput.style.borderBottomColor = "#3C3C3C";
             return true;
         }
     };
@@ -65,20 +54,42 @@ const Sign = () => {
             </header>
             <section className="login">
                 <h2 className="login-header">Załóż konto</h2>
-                <div className="login-decoration" style={{backgroundImage: `url(${process.env.PUBLIC_URL}/assets/Decoration.svg`}}/>
+                <div className="login-decoration"
+                     style={{backgroundImage: `url(${process.env.PUBLIC_URL}/assets/Decoration.svg`}}/>
                 <form className="login-form" onSubmit={handleSubmit} noValidate>
                     <label htmlFor="email">Email</label>
-                    <input id="email" type="email" name="email" value={email} onChange={e=> setEmail(e.target.value)} />
+                    <input
+                        className={emailErr !== "" ? "error-input" : ""}
+                        id="email"
+                        type="email"
+                        name="email"
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                    />
                     <div className="error-msg">{emailErr}</div>
                     <label htmlFor="password">Hasło</label>
-                    <input id="password" type="password" name="password" value={password} onChange={e => setPassword(e.target.value)} />
+                    <input
+                        className={passwordErr !== "" ? "error-input" : ""}
+                        id="password"
+                        type="password"
+                        name="password"
+                        value={password}
+                        onChange={e => setPassword(e.target.value)}
+                    />
                     <div className="error-msg">{passwordErr}</div>
                     <label htmlFor="passwordRepeat">Powtórz hasło</label>
-                    <input id="passwordRepeat" type="password" name="passwordRepeat" value={passwordRepeat} onChange={e => setPasswordRepeat(e.target.value)} />
+                    <input
+                        className={passwordRepeatErr !== "" ? "error-input" : ""}
+                        id="passwordRepeat"
+                        type="password"
+                        name="passwordRepeat"
+                        value={passwordRepeat}
+                        onChange={e => setPasswordRepeat(e.target.value)}
+                    />
                     <div className="error-msg">{passwordRepeatErr}</div>
                     <div className="login-buttons">
                         <Link to="/logowanie">Zaloguj się</Link>
-                        <input type="submit" value="Załóż konto" />
+                        <input type="submit" value="Załóż konto"/>
                     </div>
                 </form>
             </section>
